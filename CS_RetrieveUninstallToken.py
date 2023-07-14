b8f16ff2193bc6c1d4610481ab312c9d7ff668de
Marq-Dev <marq.mercado@gmail.com>
2023-07-14 07:30:42 -0500

    Update CS_RetrieveUninstallToken.py

--- CS_RetrieveUninstallToken.py
+++ CS_RetrieveUninstallToken.py
@@ -5,34 +5,39 @@ from falconpy import SensorUpdatePolicy, Hosts
 def query_api():
  
     # Get content from entry box widget
-    cs_client = str(client_field.get())
-    cs_secret = str(secret_field.get())
-    cs_host = str(host_field.get())
-    cs_comment = str(comment_field.get())
+    cs_client = str(client_field.get().strip())
+    cs_secret = str(secret_field.get().strip())
+    cs_host = str(host_field.get().strip())
+    cs_comment = str(comment_field.get().strip())
     
     # Sets the Client ID and Secret for the API endpoints
-    hosts_endpoint = Hosts(client_id=cs_client, client_secret=cs_secret)
-    sensor_endpoint = SensorUpdatePolicy(client_id=cs_client, client_secret=cs_secret)
+    hosts_endpoint = Hosts(client_id = cs_client, client_secret = cs_secret)
+    sensor_endpoint = SensorUpdatePolicy(client_id = cs_client, client_secret = cs_secret)
 
     # Queries the API using the Device Hostname
     # Response returned as a dict, which gets filtered down using keys
-    cs_aid = hosts_endpoint.query_devices_by_filter(filter=f"hostname:'{cs_host}*'")["body"]["resources"][0]
+    cs_aid = hosts_endpoint.query_devices_by_filter(filter = f"hostname:'{cs_host}*'")["body"]["resources"][0]
 
     # Queries the API using the Agent ID and submits a comment for audit logs
-    cs_token = sensor_endpoint.reveal_uninstall_token(audit_message=cs_comment, device_id=cs_aid)["body"]["resources"][0]["uninstall_token"]
+    cs_token = sensor_endpoint.reveal_uninstall_token(audit_message = cs_comment, device_id = cs_aid)["body"]["resources"][0]["uninstall_token"]
 
     # Inserts the response_token value in the entry box widget.
     token_field.insert(0, cs_token)
  
+# Function for copying the current entry box widget value to the clipboard
+def copy_to_clipboard():
+    root.clipboard_clear()
+    root.clipboard_append(token_field.get().strip())
+
 # Function for clearing the contents of all entry box widgets
 def clear_all():
  
     # Deletes all content inside entry box widgets
-    client_field.delete(0, 'end')
-    secret_field.delete(0, 'end')
-    host_field.delete(0, 'end')
-    comment_field.delete(0, 'end')
-    token_field.delete(0, 'end')
+    client_field.delete(0, tk.END)
+    secret_field.delete(0, tk.END)
+    host_field.delete(0, tk.END)
+    comment_field.delete(0, tk.END)
+    token_field.delete(0, tk.END)
    
     # Sets focus on the client_field entry box widget
     client_field.focus_set()
@@ -46,19 +51,15 @@ if __name__ == "__main__":
     # Sets the background color of GUI window
     root.configure(background = 'white')
 
+    # Sets the initial size of GUI window
+    root.geometry("")
+
     # Sets the name of GUI window
     root.title("CrowdStrike Uninstall Token Retriever")
 
     # Sets the scale relative to other widgets
-    root.rowconfigure(0, weight=1)
-    root.rowconfigure(1, weight=1)
-    root.rowconfigure(2, weight=1)
-    root.rowconfigure(3, weight=1)
-    root.rowconfigure(4, weight=1)
-    root.rowconfigure(5, weight=1)
-    root.rowconfigure(6, weight=1)
-    root.columnconfigure(0, weight=1)
-    root.columnconfigure(1, weight=1)
+    root.rowconfigure((0, 1, 2, 3, 4, 5, 6), weight = 1)
+    root.columnconfigure((0, 1, 2), weight = 1)
        
     # Creates label widgets
     label_1 = tk.Label(root, text = "API Client : ", fg = 'black', bg = 'white')
@@ -68,11 +69,11 @@ if __name__ == "__main__":
     label_5 = tk.Label(root, text = "Uninstall Token : ", fg = 'black', bg = 'white')
  
     # Grid method is used for placing the widgets at respective positions in table like structure
-    label_1.grid(row = 0, column = 0, padx = 10, pady = 10, sticky="NSEW")
-    label_2.grid(row = 1, column = 0, padx = 10, pady = 10, sticky="NSEW")
-    label_3.grid(row = 2, column = 0, padx = 10, pady = 10, sticky="NSEW")
-    label_4.grid(row = 3, column = 0, padx = 10, pady = 10, sticky="NSEW")
-    label_5.grid(row = 5, column = 0, padx = 10, pady = 10, sticky="NSEW")
+    label_1.grid(row = 0, column = 0, padx = (10, 0), pady = 5, sticky = "NSE")
+    label_2.grid(row = 1, column = 0, padx = (10, 0), pady = 5, sticky = "NSE")
+    label_3.grid(row = 2, column = 0, padx = (10, 0), pady = 5, sticky = "NSE")
+    label_4.grid(row = 3, column = 0, padx = (10, 0), pady = 5, sticky = "NSE")
+    label_5.grid(row = 5, column = 0, padx = (10, 0), pady = 5, sticky = "NSE")
 
     # Creates entry box widgets for filling or typing the information
     client_field = tk.Entry(root)
@@ -81,18 +82,20 @@ if __name__ == "__main__":
     comment_field = tk.Entry(root)
     token_field = tk.Entry(root)
  
-    client_field.grid(row = 0, column = 1, padx = 10, pady = 10, sticky="EW")
-    secret_field.grid(row = 1, column = 1, padx = 10, pady = 10, sticky="EW")
-    host_field.grid(row = 2, column = 1, padx = 10, pady = 10, sticky="EW")
-    comment_field.grid(row = 3, column = 1, padx = 10, pady = 10, sticky="EW")
-    token_field.grid(row = 5, column = 1, padx = 10, pady = 10, sticky="EW")
+    client_field.grid(row = 0, column = 1, columnspan = 2, padx = (0, 10), pady = 5, sticky = "EW")
+    secret_field.grid(row = 1, column = 1, columnspan = 2, padx = (0, 10), pady = 5, sticky = "EW")
+    host_field.grid(row = 2, column = 1, columnspan = 2, padx = (0, 10), pady = 5, sticky = "EW")
+    comment_field.grid(row = 3, column = 1, columnspan = 2, padx = (0, 10), pady = 5, sticky = "EW")
+    token_field.grid(row = 5, column = 1, padx = 0, pady = 5, sticky="W")
 
     # Creates button widgets linked to the query_api and clear_all functions
-    button_1 = tk.Button(root, text = "Submit", bg = "white", fg = "black", command = query_api)
-    button_2 = tk.Button(root, text = "Clear", bg = "white", fg = "black", command = clear_all)
+    button_1 = tk.Button(root, text = "Submit", bg = "white", fg = "black", width = 6, command = query_api)
+    button_2 = tk.Button(root, text = "Clear", bg = "white", fg = "black", width = 6, command = clear_all)
+    button_3 = tk.Button(root, text = "Copy", bg = "white", fg = "black", width = 1, command = copy_to_clipboard)
 
-    button_1.grid(row = 4, column = 1, pady = 10)
-    button_2.grid(row = 6, column = 1, pady = 10)
+    button_1.grid(row = 4, column = 1, padx = 0, pady = 5, sticky = "W")
+    button_2.grid(row = 4, column = 1, padx = 0, pady = 5, sticky = "E")
+    button_3.grid(row = 5, column = 2, padx = (5, 10), pady = 5)
     
     # Starts the GUI
     root.mainloop()
