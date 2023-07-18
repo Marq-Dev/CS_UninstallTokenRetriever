6d8752c9e4560f56ef76b336262a4f9083a8a394
Marq-Dev <marq.mercado@gmail.com>
2023-07-18 12:52:53 -0500

    Update CS_UninstallTokenRetriever.py

--- CS_UninstallTokenRetriever.py
+++ CS_UninstallTokenRetriever.py
@@ -1,37 +1,61 @@
 #!/usr/bin/env python3
 
 import tkinter as tk
-import falconpy as fp
+from tkinter import messagebox
+from falconpy import Hosts, SensorUpdatePolicy
 
 # Function for querying the CrowdStrike API
 def query_api():
+    try:
+        # Get content from entry box widget
+        cs_client = str(client_field.get().strip())
+        cs_secret = str(secret_field.get().strip())
+        cs_host = str(host_field.get().strip())
+        cs_comment = str(comment_field.get().strip())
 
-    # Get content from entry box widget
-    cs_client = str(client_field.get().strip())
-    cs_secret = str(secret_field.get().strip())
-    cs_host = str(host_field.get().strip())
-    cs_comment = str(comment_field.get().strip())
+        if not all([cs_client, cs_secret, cs_host, cs_comment]):
+            messagebox.showerror("Error", "All fields are required!")
+            return
 
-    # Sets the Client ID and Secret for the API endpoints
-    hosts_endpoint = fp.Hosts(client_id = cs_client, client_secret = cs_secret)
-    sensor_endpoint = fp.SensorUpdatePolicy(client_id = cs_client, client_secret = cs_secret)
+        # Sets the Client ID and Secret for the API endpoints
+        hosts_endpoint = Hosts(client_id = cs_client, client_secret = cs_secret)
+        sensor_endpoint = SensorUpdatePolicy(client_id = cs_client, client_secret = cs_secret)
 
-    # Queries the API for the Agent ID using the Device Hostname
-    # Response returned as a dict, which gets filtered down using keys
-    cs_aid = hosts_endpoint.query_devices_by_filter(filter = f"hostname:'{cs_host}*'")["body"]["resources"][0]
+        # Queries the API for the Agent ID using the Device Hostname
+        # Response returned as a dict, which gets filtered down using keys
+        hosts_response = hosts_endpoint.query_devices_by_filter(filter = f"hostname:'{cs_host}*'")
 
-    # Queries the API for the uninstall token using the Agent ID and submits a comment for audit logs
-    cs_token = sensor_endpoint.reveal_uninstall_token(audit_message = cs_comment, device_id = cs_aid)["body"]["resources"][0]["uninstall_token"]
+        if hosts_response["status_code"] == 200:
+            cs_aid = hosts_response["body"]["resources"][0]
+        else:
+            for error_result in hosts_response["body"]["errors"]:
+                messagebox.showerror("Error", str(error_result["message"]))
+            return
 
-    # Inserts the response_token value in the entry box widget.
-    token_field.insert(0, cs_token)
+        # Queries the API for the uninstall token using the Agent ID and submits a comment for audit logs
+        sensor_response = sensor_endpoint.reveal_uninstall_token(audit_message = cs_comment, device_id = cs_aid)
+
+        if sensor_response["status_code"] == 200:
+            cs_token = sensor_response["body"]["resources"][0]["uninstall_token"]
+        else:
+            for error_result in sensor_response["body"]["errors"]:
+                messagebox.showerror("Error", str(error_result["message"]))
+            return
+
+        # Inserts the response_token value in the entry box widget.
+        token_field.insert(0, cs_token)
+    
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
 
 # Function for copying the current entry box widget value to the clipboard
 def copy_to_clipboard():
-
-    # Clears the clipboard and sets it to the token field value
-    window.clipboard_clear()
-    window.clipboard_append(token_field.get().strip())
+    try:
+        # Clears the clipboard and sets it to the token field value
+        window.clipboard_clear()
+        window.clipboard_append(token_field.get().strip())
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
 
 # Function for clearing the contents of all entry box widgets
 def clear_all():
